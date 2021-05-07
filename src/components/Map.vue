3c42debc3baeb9bdc4ebb73a1de25093d474d419
Sam <samkey726@gmail.com>
2021-05-07 01:30:59 +0100

    map:D

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,52 @@
 <template>
-  <div id="content">
-      <h1 id="title">Map</h1>
-  </div>
+    <div id="container">
+        <div id="mapContainer"></div>
+    </div>
 </template>
 
 <script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
 export default {
-  name: 'Map'
-}
+ name: "Map",
+ data() {
+   return{
+     center: [37,7749, -122,4194]
+   }},
+ methods: {
+   setupLeafletMap: function () {
+     const map = L.map('mapContainer', {
+         zoomControl: false,
+         maxBounds: [[53.9019693, -106.4279718], [-54.5276119, 107.25057270]]
+     }).setView([8.608, 30.961], 5);
+
+
+     var surfaceLayer = L.tileLayer('https://map-543f.kxcdn.com/osrs-world-map/v2/{z}/{x}/{y}.png', {
+            maxZoom: 6,
+            minZoom: 4,
+            continuousWorld: false,
+            noWrap: true,
+     }).addTo(map);
+
+
+     console.log(map)
+     console.log(surfaceLayer)
+   },
+ },
+ mounted() {
+   this.setupLeafletMap();
+ },
+};
 </script>
 
-<style>
+<style scoped>
+#mapContainer {
+    top: 43px;
+    left: 86px;
+    position: absolute;
+    width: 95.5vw;
+    height: 105.85vh;
 
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+
 Vue.config.productionTip = false
+
 
 Vue.use(VueRouter)
 
